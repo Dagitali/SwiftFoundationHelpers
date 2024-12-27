a407b51f0445ec059821b1436bd84349cc80c967
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2024-12-27 06:56:07 -0500

    feat(string): Update StringExtensions.swift
    Add subsection headers.
    Convert parameterless methods to computed vars.
    Create 2 computed vars & 1 method for string validation.

--- Sources/SwiftFoundationHelpers/Extensions/StringExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/StringExtensions.swift
@@ -17,18 +17,74 @@ import Foundation
 // MARK: - Public
 
 public extension String {
+    // MARK: Comparison
+
     /// Determines whether the string matches a given regular expression pattern.
     ///
     /// - Parameter regex: A string containing the regular expression pattern.
     /// - Returns: A Boolean value indicating whether the string matches the pattern.
     func matches(_ regex: String) -> Bool {
-        return range(of: regex, options: .regularExpression) != nil
+        range(of: regex, options: .regularExpression) != nil
     }
+
+    // MARK: Trimming
 
     /// Trims leading and trailing whitespace and newline characters from the string.
     ///
     /// - Returns: A new string with whitespace and newline characters removed from both ends.
-    func trimmed() -> String {
-        return trimmingCharacters(in: .whitespacesAndNewlines)
+    var trimmed: String {
+        trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+    // MARK: Validation
+
+    /// Validates whether the string is a properly formatted email address.
+    ///
+    /// The email validation is based on the following pattern:
+    /// - Starts with alphanumeric characters, including optional special characters.
+    /// - Followed by an `@` symbol.
+    /// - Ends with a valid domain name and top-level domain.
+    ///
+    /// - Returns: `true` if the string matches the email format; `false` if not.
+    var isValidEmail: Bool {
+        let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        return predicate.evaluate(with: self)
+    }
+
+    /// Validates whether the string is a strong password.
+    ///
+    /// A valid password must meet the following criteria:
+    /// - Contains at least one uppercase letter.
+    /// - Contains at least one lowercase letter.
+    /// - Contains at least one numeric digit.
+    /// - Contains at least one special character (`#?!@$%^&*-`).
+    /// - Has a minimum length of 8 characters.
+    ///
+    /// - Returns: `true` if the string meets the password strength criteria; `false` if not.
+    var isValidPassword: Bool {
+        let regularExpression = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        return predicate.evaluate(with: self)
+    }
+
+    /// Validates whether the string is a valid phone number.
+    ///
+    /// The phone number validation is based on the following pattern:
+    /// - Starts with a `0`.
+    /// - Followed by numeric characters only.
+    ///
+    /// - Returns: `true` if the string matches the phone number format; `false` if not.
+    var isValidPhone: Bool {
+        let regularExpression = "0[0-9]*"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        return predicate.evaluate(with: self)
+    }
+
+    /// Checks if a string is empty or contains only whitespace characters.
+    ///
+    /// - Returns: `true` if the string is empty or contains only whitespace; `false` if not.
+    func isEmptyOrWhitespace(_ regex: String) -> Bool {
+        trimmed.isEmpty
     }
 }
