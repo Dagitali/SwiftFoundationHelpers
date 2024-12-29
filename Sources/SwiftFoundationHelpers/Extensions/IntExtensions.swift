7624bdc9bdce88b92f0671edcc6695b2a8a32d59
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2024-12-29 07:54:11 -0500

    docs(int): Update IntExtension.swift
    Add example(s) to doc comments.

--- Sources/SwiftFoundationHelpers/Extensions/IntExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/IntExtensions.swift
@@ -19,12 +19,33 @@ import Foundation
 public extension Int {
     /// Generates a range from 0 to the current integer (exclusive).
     ///
+    /// Example:
+    /// ```swift
+    /// let range = 0.range()
+    /// print(range) // Output: 0..<0
+    ///
+    /// let range = 5.range()
+    /// print(range) // Output: 0..<5
+    ///
+    /// let array = Array(range)
+    /// print(array) // Output: [0, 1, 2, 3, 4]
+    /// ```
+    ///
     /// - Returns: A `Range<Int>` from 0 to the integer.
     func range() -> Range<Int> {
-        return 0..<self
+        0..<self
     }
 
     /// Repeats a closure a specified number of times.
+    ///
+    /// Example:
+    /// ```swift
+    /// var sum = 0
+    /// 3.times {
+    ///     sum += 1
+    /// }
+    /// print(sum) // Output: 3
+    /// ```
     ///
     /// - Parameter action: The closure to execute.
     func times(_ action: () -> Void) {
