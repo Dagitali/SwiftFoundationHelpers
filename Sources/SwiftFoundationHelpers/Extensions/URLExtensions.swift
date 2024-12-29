8e1ab274a7dfaca5e11afbc15ef3680a30a59a66
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2024-12-29 07:54:39 -0500

    docs(url): Update URLExtension.swift
    Add example(s) to doc comments.
    Insert newline before each `return` statement where missing.

--- Sources/SwiftFoundationHelpers/Extensions/URLExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/URLExtensions.swift
@@ -19,6 +19,18 @@ import Foundation
 public extension URL {
     /// Adds or updates query parameters to the URL.
     ///
+    /// Example:
+    /// ```swift
+    /// if let url = URL(string: "https://example.com") {
+    ///     let updatedURL = url.addingQueryParameters(["key": "value", "foo": "bar"])
+    ///     print(updatedURL) // Output: "https://example.com?key=value&foo=bar"
+    ///
+    ///     let existingURL = URL(string: "https://example.com?existing=value")
+    ///     let newURL = existingURL?.addingQueryParameters(["newKey": "newValue"])
+    ///     print(newURL) // Output: "https://example.com?existing=value&newKey=newValue"
+    /// }
+    /// ```
+    ///
     /// - Parameter parameters: A dictionary of query parameters to add or update.
     /// - Returns: A new URL with the updated query parameters.
     func addingQueryParameters(_ parameters: [String: String]) -> URL? {
@@ -28,15 +40,26 @@ public extension URL {
             queryItems.append(URLQueryItem(name: key, value: value))
         }
         components?.queryItems = queryItems
+
         return components?.url
     }
 
     /// Retrieves the value of a query parameter from the URL.
     ///
+    /// Example:
+    /// ```swift
+    /// if let url = URL(string: "https://example.com?key=value&foo=bar") {
+    ///     print(url.queryParameter(for: "key")) // Output: "value"
+    ///     print(url.queryParameter(for: "foo")) // Output: "bar"
+    ///     print(url.queryParameter(for: "missing")) // Output: nil
+    /// }
+    /// ```
+    ///
     /// - Parameter key: The key of the query parameter.
     /// - Returns: The value of the query parameter as a string, or `nil` if it does not exist.
     func queryParameter(for key: String) -> String? {
         let components = URLComponents(url: self, resolvingAgainstBaseURL: false)
+
         return components?.queryItems?.first(where: { $0.name == key })?.value
     }
 }
