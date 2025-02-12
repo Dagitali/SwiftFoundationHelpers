621869231c5fbe5c6bce646ac2bcddbe7598c481
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-02-11 20:14:45 -0500

    feat(string): Update StringExtensions.swift
    Limit doc comments to under 80 chars per line.
    Add 4 instance methods.

--- Sources/SwiftFoundationHelpers/Extensions/StringExtensions.swift
+++ Sources/SwiftFoundationHelpers/Extensions/StringExtensions.swift
@@ -24,7 +24,8 @@ public extension String {
 
     /// Checks if the string is empty or contains only whitespace characters.
     ///
-    /// - Returns: `true` if the string is empty or contains only whitespace; `false` if not.
+    /// - Returns: `true` if the string is empty or contains only whitespace;
+    ///   `false` if not.
     ///
     /// ## Example
     /// ```swift
@@ -83,7 +84,8 @@ public extension String {
     /// ```
     ///
     /// - Parameter regex: A string containing the regular expression pattern.
-    /// - Returns: A Boolean value indicating whether the string matches the pattern.
+    /// - Returns: A Boolean value indicating whether the string matches the
+    ///   pattern.
     func matches(_ regex: String) -> Bool {
         range(of: regex, options: .regularExpression) != nil
     }
@@ -130,9 +132,11 @@ public extension String {
         split(separator: " ").reversed().joined(separator: " ")
     }
 
-    /// Trims leading and trailing whitespace and newline characters from the string.
+    /// Trims leading and trailing whitespace and newline characters from the
+    /// string.
     ///
-    /// - Returns: A new string with whitespace and newline characters removed from both ends.
+    /// - Returns: A new string with whitespace and newline characters removed
+    ///   from both ends.
     ///
     /// ## Example
     /// ```swift
@@ -144,16 +148,132 @@ public extension String {
         trimmingCharacters(in: .whitespacesAndNewlines)
     }
 
+    // MARK: Matching
+
+    /// Finds the closest match in an array of strings using the Levenshtein
+    /// distance.
+    /// - Parameter list: An array of strings to compare against.
+    /// - Returns: The closest matching string if found within the acceptable
+    ///   distance.
+    ///
+    /// ## Example
+    /// ```swift
+    /// let words = ["apple", "table", "car"]
+    /// let userInput = "appl"
+    /// if let closestMatch = userInput.matchClosest(in: words) {
+    ///     print("Did you mean: \(closestMatch)?")
+    /// }
+    /// ```
+    func matchClosest(in list: [String]) -> String? {
+        let maxDistance = 2  // Adjust based on tolerance for spelling errors
+        var bestMatch: (String, Int)? = nil
+
+        for word in list {
+            let distance = self.levenshteinDistance(to: word)
+            if distance <= maxDistance, (bestMatch == nil || distance < bestMatch!.1) {
+                bestMatch = (word, distance)
+            }
+        }
+
+        return bestMatch?.0
+    }
+
+    /// Finds the closest key match in a given dictionary using the Levenshtein
+    /// distance.
+    ///
+    /// - Parameter dictionary: A dictionary where keys are strings to compare
+    ///   against and values are associated data.
+    /// - Returns: The value associated with the closest matching key, if found
+    ///   within the acceptable distance.
+    ///
+    /// ## Example
+    /// ```swift
+    /// let dataDictionary = ["apple": "fruit", "table": "furniture", "car": "vehicle"]
+    /// let userInput = "appl"
+    /// if let closestMatch = userInput.matchClosest(in: dataDictionary) {
+    ///     print("Did you mean: \(closestMatch)?")
+    /// }
+    /// ```
+    func matchClosest<T>(in dictionary: [String: T]) -> T? {
+        let keys = Array(dictionary.keys)
+        if let bestKey = self.matchClosest(in: keys) {
+            return dictionary[bestKey]
+        }
+        return nil
+    }
+
+    /// Finds the closest match in an enum that conforms to CaseIterable and
+    /// has a String raw value.
+    ///
+    /// - Parameter enumType: A CaseIterable enum type where each case has a
+    ///   String raw value.
+    /// - Returns: The closest matching enum case if found within the
+    ///   acceptable distance.
+    ///
+    /// Example:
+    /// ```swift
+    /// enum Fruit: String, CaseIterable {
+    ///     case apple, banana, cherry
+    /// }
+    ///
+    /// let userInput = "appl"
+    /// if let closestMatch = userInput.matchClosest(in: Fruit.self) {
+    ///     print("Did you mean: \(closestMatch.rawValue)?")
+    /// }
+    /// ```
+    func matchClosest<T: CaseIterable & RawRepresentable>(in enumType: T.Type) -> T? where T.RawValue == String {
+        let cases = enumType.allCases.map { $0.rawValue }
+        if let bestRawValue = self.matchClosest(in: cases) {
+            return enumType.allCases.first { $0.rawValue == bestRawValue }
+        }
+        return nil
+    }
+
+    /// Calculates the Levenshtein distance between the current string and another string.
+    ///
+    /// - Parameter other: The string to compare against.
+    /// - Returns: The Levenshtein distance between the two strings.
+    ///
+    /// ## Example
+    /// ```swift
+    /// let distance = "kitten".levenshteinDistance(to: "sitting")
+    /// print(distance) // Outputs: 3
+    /// ```
+    func levenshteinDistance(to other: String) -> Int {
+        let lhsCount = self.count
+        let rhsCount = other.count
+
+        if lhsCount == 0 { return rhsCount }
+        if rhsCount == 0 { return lhsCount }
+
+        var matrix = Array(repeating: Array(repeating: 0, count: rhsCount + 1), count: lhsCount + 1)
+
+        for i in 0...lhsCount { matrix[i][0] = i }
+        for j in 0...rhsCount { matrix[0][j] = j }
+
+        for i in 1...lhsCount {
+            for j in 1...rhsCount {
+                let cost = self[self.index(self.startIndex, offsetBy: i - 1)] ==
+                           other[other.index(other.startIndex, offsetBy: j - 1)] ? 0 : 1
+                matrix[i][j] = Swift.min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + cost)
+            }
+        }
+
+        return matrix[lhsCount][rhsCount]
+    }
+
     // MARK: Validation
 
     /// Validates whether the string is a properly formatted email address.
     ///
     /// The email validation is based on the following pattern:
-    /// - Starts with alphanumeric characters, including optional special characters.
+    /// - Starts with alphanumeric characters, including optional special
+    //    characters.
     /// - Followed by an `@` symbol.
     /// - Ends with a valid domain name and top-level domain.
     ///
-    /// - Returns: `true` if the string matches the email format; `false` if not.
+    /// - Returns: `true` if the string matches the email format; `false` if
+    ///   not.
     ///
     /// ## Example
     /// ```swift
@@ -176,7 +296,8 @@ public extension String {
     /// - Contains at least one special character (`#?!@$%^&*-`).
     /// - Has a minimum length of 8 characters.
     ///
-    /// - Returns: `true` if the string meets the password strength criteria; `false` if not.
+    /// - Returns: `true` if the string meets the password strength criteria;
+    ///  `false` if not.
     ///
     /// ## Example
     /// ```swift
@@ -196,7 +317,8 @@ public extension String {
     /// - Starts with a `0`.
     /// - Followed by numeric characters only.
     ///
-    /// - Returns: `true` if the string matches the phone number format; `false` if not.
+    /// - Returns: `true` if the string matches the phone number format;
+    /// `false` if not.
     ///
     /// ## Example
     /// ```swift
