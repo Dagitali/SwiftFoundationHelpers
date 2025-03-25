c4862f1c0563bc35ed2fb3d216b703786f5f475a
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2025-03-25 19:42:31 -0400

    test(url): Update URLExtensionsTests.swift
    Add unit test for each new computed prop.
    Standardize styling of Swift documentation comments.
    Remove trailing whitespace.

--- Tests/SwiftFoundationHelpersTests/Extensions/URLExtensionsTests.swift
+++ Tests/SwiftFoundationHelpersTests/Extensions/URLExtensionsTests.swift
@@ -31,9 +31,28 @@ private struct MockModel: Codable, Equatable {
 /// A test suite to validate the functionality of  `URL` extensions.
 @Suite("URLExtensions Tests")
 struct URLExtensionsTests {
+    // MARK: Favicons
+
+    /// Tests the `favicon` computed property.
+    /// This ensures the property correctly construct the favicon's URL .
+    @Test
+    func faviconComputedProperty() {
+        // Given...
+        let url = URL(string: "https://www.example.com")!
+
+        // When...
+        let faviconURL = url.favicon
+
+        // Then...
+        #expect(
+            faviconURL?.absoluteString == "https://www.example.com/favicon.ico",
+            "Favicon URL should be 'https://www.example.com/favicon.ico'."
+        )
+    }
+
     // MARK: JSON
-    
-    /// Tests the `decode()` method.
+
+    /// Tests the `decode()` instance method.
     ///
     /// This ensures the method correctly decodes the JSON file into the
     /// expected model.
@@ -44,10 +63,10 @@ struct URLExtensionsTests {
             Issue.record("Failed to locate test JSON file.")
             return
         }
-        
+
         // When...
         let decodedModel: MockModel? = testFileURL.decode(as: MockModel.self)
-        
+
         // Then...
         #expect(
             decodedModel != nil,
@@ -58,8 +77,8 @@ struct URLExtensionsTests {
             "Decoded model does not match expected value."
         )
     }
-    
-    /// Tests the `encode()` method.
+
+    /// Tests the `encode()` instance method.
     ///
     /// This ensures the method correctly encodes the model into a JSON file.
     @Test
@@ -67,57 +86,76 @@ struct URLExtensionsTests {
         // Given...
         let outputURL = FileManager.default.temporaryDirectory.appendingPathComponent("output.json")
         let modelToEncode = MockModel(id: 42, name: "Encoded Object")
-        
+
         // When...
         outputURL.encode(modelToEncode)
-        
+
         // Then...
         #expect(
             FileManager.default.fileExists(atPath: outputURL.path) == true,
             "Encoded file should exist."
         )
-        
+
         // When...
         let decodedModel: MockModel? = outputURL.decode(as: MockModel.self)
-        
+
         // THen...
         #expect(
             decodedModel == modelToEncode,
             "Decoded model should match the encoded model."
         )
     }
-    
+
     // MARK: Queries
-    
-    /// Tests the `addingQueryParameters()` method.
+
+
+    /// Tests the `queryParameters` computed property.
+    /// This ensures the property correctly returns a dictionary of query items
+    /// parsed from the URL's query string.
+    @Test()
+    func queryParameters() {
+        // Given...
+        let url = URL(string: "https://example.com?foo=bar&baz=qux")!
+
+        // When...
+        let parameters = url.queryParameters
+
+        // Then...
+        #expect(
+            parameters == ["foo": "bar", "baz": "qux"],
+            "The queryParameters property should return the expected dictionary of query items."
+        )
+    }
+
+    /// Tests the `appending()` instance  method.
     ///
     /// This ensures the method correctly adds or updates query parameters to
     /// the URL.
     @Test
-    func addingQueryParameters() {
+    func appendingQueryParameters() {
         // Given...
         let baseURL = URL(string: "https://example.com")!
-        
+
         // When...
-        let newURL = baseURL.addingQueryParameters(["key": "value"])
-        
+        let newURL = baseURL.appending(queryParameters: ["key": "value"])
+
         // Then...
-        #expect(newURL?.absoluteString == "https://example.com?key=value")
-        
+        #expect(newURL.absoluteString == "https://example.com?key=value")
+
         // Given...
-        let existingURL = newURL!
-        
+        let existingURL = newURL
+
         // When...
-        let updatedURL = existingURL.addingQueryParameters(["new-key": "new-param"])
-        
+        let updatedURL = existingURL.appending(queryParameters: ["new-key": "new-param"])
+
         // Then...
         #expect(
-            updatedURL?.absoluteString == "https://example.com?key=value&new-key=new-param" ||
-            updatedURL?.absoluteString == "https://example.com?foo=bar&new-key=new-param"
+            updatedURL.absoluteString == "https://example.com?key=value&new-key=new-param" ||
+            updatedURL.absoluteString == "https://example.com?foo=bar&new-key=new-param"
         )
     }
-    
-    /// Tests the `queryParameter()` method.
+
+    /// Tests the `queryParameter()` instance  method.
     ///
     /// This ensures the method correctly retrieves the value of a query
     /// parameter from the URL.
@@ -130,10 +168,10 @@ struct URLExtensionsTests {
     func queryParameter(key: String, expected: String) {
         // Given...
         let url = URL(string: "https://example.com?key=value&new-key=new-value")!
-        
+
         // When...
         let actual = url.queryParameter(for: key)
-        
+
         // Then...
         #expect(
             actual == expected,
@@ -143,7 +181,7 @@ struct URLExtensionsTests {
         )
     }
 
-    /// Tests the `queryParameter()` method.
+    /// Tests the `queryParameter()` instance  method.
     ///
     /// This ensures the method correctly retrieves the value of a query
     /// parameter from the URL.
@@ -161,6 +199,40 @@ struct URLExtensionsTests {
             actual == expected,
             """
             The query parameter value should be "\(expected)", not "\(actual)".
+            """
+        )
+    }
+
+    // MARK: Schemes
+
+    /// Tests the `isHTTP` computed property.
+    ///
+    /// This ensures the property correctly identifies URLs that use HTTP or
+    /// HTTPS schemes.
+    @Test(
+        arguments: zip(
+            [
+                "https://example.com",
+                "http://example.com",
+                "ftp://example.com",
+            ],
+            [
+                true, true, false
+            ]
+        )
+    )
+    func isHTTPComputedProperty(urlString: String, expected: Bool) {
+        // Given...
+        let url = URL(string: urlString)!
+
+        // When...
+        let actual = url.isHTTP
+
+        // Then...
+        #expect(
+            actual == expected,
+            """
+            The URL's schema should \(expected ? "be" : "not be") of type "HTTPS" or "HTTP".
             """
         )
     }
